b4bcb7b45b11c9547badba53fc28fb569d93c40f
Fatma Belhaj <fatmazahra.benhajminiaoui@esprit.tn>
2024-02-15 12:14:34 +0100

    alerts, mounting and unmounting

--- src/Components/Event.jsx
+++ src/Components/Event.jsx
@@ -1,28 +1,39 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 export default function Event(props) {
-    const[e,setEvent]=useState(props.event)
-    const bookEvent=()=>{
-        props.fnAlert();
-        setEvent((pe)=>({...pe,
-            nbTickets: pe.nbTickets-1,
-            nbParticipants:pe.nbParticipants+1}))
-    }
+  const [e, setEvent] = useState(props.event);
+  const changeLike = () => {
+    setEvent((pe) => ({ ...pe, like: !pe.like }));
+  };
+  const bookEvent = () => {
+    props.fnAlert();
+    setEvent((pe) => ({
+      ...pe,
+      nbTickets: pe.nbTickets - 1,
+      nbParticipants: pe.nbParticipants + 1,
+    }));
+  };
 
-    
   return (
     <>
       <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={`images/${e.img}`} />
+        <Card.Img
+          variant="top"
+          src={e.nbTickets == 0 ? "images/sold_out.png" : `images/${e.img}`}
+        />
         <Card.Body>
           <Card.Title>{e.name}</Card.Title>
-          
+
           <Card.Text>Price: {e.price}</Card.Text>
           <Card.Text>Number of tickets: {e.nbTickets}</Card.Text>
-          <Card.Text>Number of participants: {e.nbParticipants}</Card.Text>  
-          
-          <Button onClick={bookEvent}>Go somewhere</Button>
+          <Card.Text>Number of participants: {e.nbParticipants}</Card.Text>
+          <Button variant="danger" onClick={changeLike}>
+            {e.like == true ? "Like" : "Dislike"}
+          </Button>
+          <Button onClick={bookEvent} disabled={e.nbTickets === 0}>
+            Buy
+          </Button>
         </Card.Body>
       </Card>
     </>
--- src/Components/Events.jsx
+++ src/Components/Events.jsx
@@ -1,15 +1,27 @@
 import Event from "./Event";
 import listEvent from '../data/events.json'
 import { Alert, CardGroup, Row } from "react-bootstrap";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 export default function Events (){
     const [showAlert,setAlert] = useState(false)
+    const [showWelcome,setWelcome] = useState(false)
     const modifAlert=()=>{
         setAlert(true);
         setTimeout(()=>setAlert(false),3000)
     }
+    useEffect(()=>{
+        setWelcome(true);
+        setTimeout(()=>setWelcome(false),3000)
+        return ()=>{
+            console.log("unmounting")
+        }
+    },[])
     return(
      <>
+     {showWelcome &&
+     <Alert  variant="success">
+    Welcome
+    </Alert> }
       <Row xs={1} md={3} >
      
     { listEvent?.map((e,i) => {
